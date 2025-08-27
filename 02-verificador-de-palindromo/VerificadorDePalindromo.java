6314f174e9ff76931d366c58d968f1189405b622
Guilherme Neiva <guineiva51@gmail.com>
2025-08-27 19:10:44 -0300

    desafios 2 e 8 finalizados

--- 02-verificador-de-palindromo/VerificadorDePalindromo.java
+++ 02-verificador-de-palindromo/VerificadorDePalindromo.java
@@ -3,14 +3,25 @@ import java.util.Scanner;
 public class VerificadorDePalindromo {
     public static void main(String[] args) {
 
-        //Passar todas as letras maiusculas para minusculas
-        //Eliminar caracteres alfanumercicos
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite:");
         String s = scanner.nextLine();
-
-
-
         scanner.close();
+
+        StringBuilder limpa = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                limpa.append(Character.toLowerCase(c));
+            }
+        }
+
+        String sLimpa = limpa.toString();
+
+        String sInvertida = limpa.reverse().toString();
+
+        boolean palindromo = sLimpa.equals(sInvertida);
+
+        System.out.println(palindromo);
     }
 }
--- 08-merge-intervals/MergeIntervals.java
+++ 08-merge-intervals/MergeIntervals.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeIntervals {
+
     public static void main(String[] args) {
 
+        int[][] intervalos = {
+                {1, 3},
+                {2, 6},
+                {8, 10},
+                {15, 18}
+        };
+
+        List<int[]> intervalosUnidos = new ArrayList<>();
+
+        intervalosUnidos.add(intervalos[0]);
+
+        for (int i = 1; i < intervalos.length; i++) {
+            int[] ultimoIntervaloUnido = intervalosUnidos.get(intervalosUnidos.size() - 1);
+            int[] intervaloAtual = intervalos[i];
+
+            if (ultimoIntervaloUnido[1] >= intervaloAtual[0]) {
+
+                ultimoIntervaloUnido[1] = Math.max(ultimoIntervaloUnido[1], intervaloAtual[1]);
+            } else {
+                intervalosUnidos.add(intervaloAtual);
+            }
+        }
+        int[][] resultado = intervalosUnidos.toArray(new int[intervalosUnidos.size()][]);
+
+        for (int[] intervalo : resultado) {
+            System.out.print(Arrays.toString(intervalo) + " ");
+        }
     }
 }
